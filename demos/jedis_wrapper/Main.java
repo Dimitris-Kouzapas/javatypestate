ed5cceb3167c54fb5bd9021655ce0681b105500c
Roly Perera <roly.perera@dynamicaspects.org>
2015-01-09 12:04:19 +0000

    Obsolete

--- demos/jedis_wrapper/Main.java
+++
@@ -1,7 +0,0 @@
-package demos.jedis_wrapper;
-public class Main  {
-	public static void main(String[] args) {
-		Seller seller = new Seller();
-		String title = seller.receiveTitleFromBuyer1();
-	}
-}
--- demos/jedis_wrapper/Seller.java
+++
@@ -1,8 +0,0 @@
-package demos.jedis_wrapper;
-public class Seller  {
-	public Seller(){
-	}
-	String receiveTitleFromBuyer1() {
-		return "hello";
-	}
-}
